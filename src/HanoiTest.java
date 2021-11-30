57bbac9057671a245f9928516a7b9160dae927a3
KevinFerati <kevin.ferati@heig-vd.ch>
2021-11-30 14:06:40 +0100

    Simplification hanoi

--- src/HanoiTest.java
+++ src/HanoiTest.java
@@ -1,26 +1,19 @@
-//import hanoi.gui.JHanoi;
+import hanoi.gui.JHanoi;
 import hanoi.Hanoi;
-import util.Pile;
-
-import java.util.Stack;
 
 public class HanoiTest {
 
     public static void main(String[] args) {
         if (args.length > 1)
-            throw new RuntimeException("max 1 argument !");
-        // Nombre de disque donne en argument -> affichage console
-        else if (args.length == 1){
-            int disques = Integer.parseInt(args[0]);
-            if (disques < 1)
-                throw new RuntimeException("Le nombre de disque doit etre > 0 !");
-
-            Hanoi h = new Hanoi(disques);
-            h.solve();
-        }
-        // Pas d'argument -> affichage graphique
-        else{
+            throw new RuntimeException("Un argument maximum");
+        
+        if (args.length == 0) {
             new JHanoi();
+            return;
         }
+
+        Hanoi h = new Hanoi(Integer.parseInt(args[0]));
+        h.solve();
+
     }
 }
